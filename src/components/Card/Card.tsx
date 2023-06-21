c6eab60420773713f962f8522a314da395bc6df2
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-06-21 10:45:52 +0200

    3rdAPI logic

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import cube from "../../assets/icon-dice.svg";
 import { CardWrapper, HeadTitle, AdviceText, StyledButton } from "./StyledCard";
+import axios from "axios";
+import { useState } from "react";
 
 const Card: React.FC = () => {
+  interface adviceProps {
+    text: string;
+    id: number;
+  }
+  const [advice, setAdvice] = useState<adviceProps>({
+    text: "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+    id: 117
+  });
+  const handleClick = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const data = response.data.slip;
+    setAdvice({...advice, text: data.advice, id: data.id})
+    console.log(advice);
+  };
+
   return (
     <CardWrapper>
-        <HeadTitle>Advice 123123</HeadTitle>
-        <AdviceText>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</AdviceText>
-        <StyledButton><img src={cube} alt="dice icon" /></StyledButton>
+      <HeadTitle>Advice #{advice.id}</HeadTitle>
+      <AdviceText>{advice.text}</AdviceText>
+      <StyledButton onClick={handleClick}>
+        <img src={cube} alt="dice icon" />
+      </StyledButton>
     </CardWrapper>
-  )
+  );
 };
 
 export default Card;
